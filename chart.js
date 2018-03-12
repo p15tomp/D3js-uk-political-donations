9ed45753fa0dc9a543655a3c3b7e1f97bd1a3efd
p15tomp <32740416+p15tomp@users.noreply.github.com>
2018-03-12 22:23:00 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -21,7 +21,7 @@ var entityCentres = {
 		individual: {x: w / 3.65, y: h / 3.3},
 	};
 
-var fill = d3.scale.ordinal().range(["#F02233", "#087FBD", "#FDBB30"]);
+var fill = d3.scale.ordinal().range(["#ff66ff", "#660033", "#c2c2a3"]);
 
 var svgCentre = { 
     x: w / 3.6, y: h / 2
